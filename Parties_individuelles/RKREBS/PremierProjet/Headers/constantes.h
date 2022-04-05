1d06ec688bb0aa3f51ee554d68a56cf82249615d
Remiik <65297161+Remiik@users.noreply.github.com>
2022-04-05 09:39:54 +0200

    Add files via upload

---
+++ Parties_individuelles/RKREBS/PremierProjet/Headers/constantes.h
@@ -0,0 +1,8 @@
+#ifndef CONSTANTES_H_INCLUDED
+#define CONSTANTES_H_INCLUDED
+
+const auto BORNE_MIN(0);
+const auto BORNE_MAX(10'000);
+const auto BORNE_MAX_FACILE(1'000);
+
+#endif // CONSTANTS
---
+++ Parties_individuelles/RKREBS/PremierProjet/Headers/menu.h
@@ -0,0 +1,8 @@
+#ifndef MENU_H_INCLUDED
+#define MENU_H_INCLUDED
+
+enum class ChoixMenu;
+void afficherMenu();
+void ChoixMenu demanderChoixMenu();
+
+#endif
