ded0b8e43c5221ea07d830524cc6c0b216c5e37a
Remiik <65297161+Remiik@users.noreply.github.com>
2022-04-05 09:51:30 +0200

    Add files via upload

---
+++ Projets_groupe/Pendu/Headers/fonctions.h
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// déclarations des fonctions
+void Bienvenue();
+void afficher_echecs(int Echecs);
+void afficher_etat(vector<char> incorrect, string Reponse);
+void FinPartie(string Reponse, string MotATrouver);
